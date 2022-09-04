c1077012e805c0c5d82a9f3c0e985784395282cb
Fahim Faisal <fahim.faisal.abir@gmail.com>
2022-09-04 18:46:21 +0600

    Server initalized

---
+++ server/app.js
@@ -0,0 +1,35 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import 'dotenv/config.js';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(
+    bodyParser.urlencoded({
+        limit: '50mb',
+        extended: true,
+        parameterLimit: 50000,
+    })
+);
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('DOCAPP API');
+});
+
+const CONNECTION_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
+    )
+    .catch((error) => console.log(`${error} did not connect`));
