16841e591996cd654bbc39e4bb67e5e594a2a4fe
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2023-08-08 20:24:24 +0600

    fixed domain issue

--- server/configs/cookie.js
+++ server/configs/cookie.js
@@ -1,19 +1,20 @@
 import urls from './urls.js';
 
 const { hostname } = new URL(urls.CLIENT_URL);
+const domain = hostname.split('.').slice(-2).join('.');
 
 const access = {
     name: process.env.COOKIE_ACCESS_NAME,
     options: {
         sameSite: 'none',
-        domain: hostname,
+        domain,
         httpOnly: false,
         secure: true,
         maxAge: process.env.ACCESS_EXP,
     },
     delete: {
         sameSite: 'none',
-        domain: hostname,
+        domain,
         httpOnly: false,
         secure: true,
         maxAge: new Date(null),
@@ -24,14 +25,14 @@ const refresh = {
     name: process.env.COOKIE_REFRESH_NAME,
     options: {
         sameSite: 'none',
-        domain: hostname,
+        domain,
         httpOnly: true,
         secure: true,
         maxAge: process.env.REFRESH_EXP,
     },
     delete: {
         sameSite: 'none',
-        domain: hostname,
+        domain,
         httpOnly: true,
         secure: true,
         maxAge: new Date(null),
